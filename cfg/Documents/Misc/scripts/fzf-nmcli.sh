21c436bbb2eb9d7a4f38e8b0dd1081da53d24295
Ian Emnace <igemnace@gmail.com>
2017-07-21 12:45:26 +0800

    tweak fzf-nmcli to look nicer

--- cfg/Documents/Misc/scripts/fzf-nmcli.sh
+++ cfg/Documents/Misc/scripts/fzf-nmcli.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+# display and prompt for wifi with fzf
 chosen_wifi=$(nmcli --terse device wifi list | cut -f 2,7,8 -d ':' | tr ':' $'\t' | FZF_DEFAULT_OPTS='--no-bold --color=fg:7,fg+:3,bg:-1,bg+:-1,hl:5,hl+:5,prompt:8,pointer:3,marker:2' fzf)
 
+# fail fast if no wifi is chosen
 [ -z "$chosen_wifi" ] && exit 1
 
-chosen_ssid=$(echo "$chosen_wifi" | cut -f 1)
-security=$(echo "$chosen_wifi" | cut -f 3)
+chosen_ssid=$(cut -f 1 <<< "$chosen_wifi")
+security=$(cut -f 3 <<< "$chosen_wifi")
 
+# check for existing NetworkManager connection
 if nmcli --terse connection show "$chosen_ssid" &>/dev/null; then
   >&2 echo "Activating existing profile for $chosen_ssid..."
   nmcli connection up "$chosen_ssid"
 else
+  # check if password is necessary
   if [[ $security =~ "WPA" ]]; then
-    read -p "Password: " password
+    read -r -p "Password: " password
 
     >&2 echo "Connecting to $chosen_ssid..."
     nmcli device wifi connect "$chosen_ssid" password "$password"
