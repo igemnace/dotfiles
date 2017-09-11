ccfa48de8ab4578f439807b22ed3458f57b0ed81
Ian Emnace <igemnace@gmail.com>
2017-09-11 18:34:38 +0800

    Send libnotify notifications for add-pass.sh
    I don't always enter the password correctly through add-pass.sh, but I
    can only find out after seeing that it does not exist through the
    rofi-pass.sh script, since the add-pass.sh window closes almost
    immediately after exiting so I can't read any error messages.
    
    So, I use notify-send to tell me whether or not the operation succeeded.

--- cfg/Documents/Misc/scripts/add-pass.sh
+++ cfg/Documents/Misc/scripts/add-pass.sh
@@ -4,4 +4,8 @@ echo "Categories:"
 ls /home/ian/.password-store/
 read -p "New pass name: " pass_name
 
-pass insert $pass_name
+if pass insert $pass_name; then
+  notify-send --urgency=normal "New password added for $pass_name."
+else
+  notify-send --urgency=normal "Failed to add password for $pass_name!"
+fi
