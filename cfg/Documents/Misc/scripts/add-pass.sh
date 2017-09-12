71f0f5ed3a6369234a35df660667b368c6d44705
Ian Emnace <igemnace@gmail.com>
2017-09-12 10:38:05 +0800

    Fix misc quoting issues in scripts
    More and more I'm getting the feeling that these scripts don't belong
    here...
    
    I'm likely going to remove my scripts from this repo, rewrite them
    properly, then upload to a different repo.

--- cfg/Documents/Misc/scripts/add-pass.sh
+++ cfg/Documents/Misc/scripts/add-pass.sh
@@ -4,7 +4,7 @@ echo "Categories:"
 ls /home/ian/.password-store/
 read -p "New pass name: " pass_name
 
-if pass insert $pass_name; then
+if pass insert "$pass_name"; then
   notify-send --urgency=normal "New password added for $pass_name."
 else
   notify-send --urgency=normal "Failed to add password for $pass_name!"
--- cfg/Documents/Misc/scripts/notify-exit.sh
+++ cfg/Documents/Misc/scripts/notify-exit.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if $*; then
+if "$@"; then
   notify-send "Done" "$*"
   exit 0
 else
   exit_code=$?
   notify-send "Exit $exit_code" "$*"
-  exit $exit_code
+  exit "$exit_code"
 fi
